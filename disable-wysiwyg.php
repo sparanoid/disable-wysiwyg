32f66ee946321933fe53289f9a7ae2fdb8ae6e24
Tunghsiao Liu <t@sparanoid.com>
2015-05-09 08:49:44 +0800

    chore: release 1.0.3

--- disable-wysiwyg.php
+++ disable-wysiwyg.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Disable WYSIWYG
  * Plugin URI:        http://sparanoid.com/work/disable-wysiwyg/
  * Description:       Disable TinyMCE Visual Editor (WYSIWYG editor) totally completely permanently forever
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Tunghsiao Liu
  * Author URI:        http://sparanoid.com/
  * Text Domain:       disable-wysiwyg
